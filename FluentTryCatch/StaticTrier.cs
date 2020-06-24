1dae832e5316b91de184732cf3c0a54ae867c56f
thomas.halin <thomas.halin.th@gmail.com>
2020-06-24 11:59:22 +0200

    Added Static Trier for type inference.

---
+++ FluentTryCatch/StaticTrier.cs
@@ -0,0 +1,15 @@
+﻿using FluentTryCatch.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentTryCatch
+{
+    public static class Trier
+    {
+        public static ICatcherOrRethrower<T, TResult> Try<T, TResult>(T content, Func<T, TResult> tryAction)
+            => new Trier<T, TResult>(content).Try(tryAction);
+    }
+}
